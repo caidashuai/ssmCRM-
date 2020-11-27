a0fc939d826f2d1749cd3ebabe17c6bd69939dac
cai <3295577683@qq.com>
2020-11-27 13:32:06 +0800

    第一次提交

---
+++ src/main/java/com/cai/servicers/customservices.java
@@ -0,0 +1,4 @@
+package com.cai.servicers;
+
+public interface customservices {
+}
---
+++ src/main/java/com/cai/servicers/servicersimpl/customservicesImpl.java
@@ -0,0 +1,8 @@
+package com.cai.servicers.servicersimpl;
+
+import com.cai.servicers.customservices;
+import org.springframework.stereotype.Service;
+
+@Service
+public class customservicesImpl implements customservices {
+}
